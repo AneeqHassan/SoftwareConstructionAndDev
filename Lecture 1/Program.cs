54d6833711c426b7ddd65d15332971e571764f4b
2312303 <2312303@PC388>
2025-09-19 09:33:33 +0500

    Add project files.

---
+++ Lecture 1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your First name: ");
+String name = Console.ReadLine();
+Console.WriteLine("Enter your Last Name: ");
+String lastName = Console.ReadLine();
+Console.WriteLine($"Hello, {name} { lastName}!");
+Console.WriteLine(String.Format("Hello, {0} {1}!", name, lastName));  
