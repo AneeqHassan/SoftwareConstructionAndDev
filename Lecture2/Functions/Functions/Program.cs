02f6b99d98d74792c8779fe83fd222bb36eb319d
2312303 <2312303@PC13>
2025-09-26 10:28:24 +0500

    lec 2 T

---
+++ Lecture2/Functions/Functions/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+static void Split(string name, out string firstNames, out string lastNames)
+{
+    int i = name.LastIndexOf(' ');
+    firstNames = name.Substring(0, i);
+    lastNames = name.Substring (i + 1);
+}
+
+string a, b;
+Split("John Q. Public", out a, out b);
+Console.WriteLine($"First Name: {a}");
+Console.WriteLine($"Last Name: {b}");
+
+
---
+++ Lecture2/Lecture2/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+string s1 = Console.ReadLine();
+string substring1 = s1.Substring(0, 10);
+string substring2 = s1.Substring(1, 10);
+Console.WriteLine($"The first 10 characters are :{substring1}");
+Console.WriteLine($"The first 10 characters are :{substring2}");
+
+char[] vowel = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+foreach (char c in vowel)
+{
+    if(s1.IndexOf(c) == -1)
+        continue;
+    if(s1.LastIndexOf(c) == -1)
+        continue;
+    Console.WriteLine($"First Index of {vowel} is s1 = {s1.IndexOf(c)}");
+    Console.WriteLine($"Last Index of {vowel} is s1 = {s1.LastIndexOf(c)}");
+}
+
+Console.WriteLine($"S1 in Lowercase : {s1.ToLower()}");
+Console.WriteLine($"S1 in Uppercase : {s1.ToUpper()}");
+
+char[] splitChar = new char[] { ' ', ',' };
+string[] spString = s1.Split(splitChar);
+
+foreach (string s in spString)
+{ 
+     Console.WriteLine(s); 
+
+}   
