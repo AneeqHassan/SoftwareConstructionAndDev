c35cb1f4e6f8a7ed85071039bee7b2a0f5b30f2a
2312303 <2312303@PC13>
2025-09-26 12:57:08 +0500

    Lec lab

---
+++ Lecture 2 Lab/Lecture 2 Lab/Program.cs
@@ -0,0 +1,90 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+//1.
+//int[][] jagged = new int[2][];
+//jagged[0] = new int[] { 1, 2, 3};
+//jagged[1] = new int[] { 4, 5 } ;
+
+//foreach (int[] row in jagged)
+//{
+
+//        foreach(var val in row)
+//        {
+//            Console.Write(val + " ");
+//        }
+//        Console.WriteLine();
+
+//    }
+
+//2.
+
+//Console.WriteLine("Enter the number of rows:");
+//int rows = int.Parse(Console.ReadLine());
+//int[][] jagged = new int[rows][];
+
+//for (int i = 0; i < rows; i++)
+//{
+
+//    Console.Write($"Enter number of elements in row {i + 1}: ");
+//    int size = int.Parse(Console.ReadLine());
+//    jagged[i] = new int[size];
+
+//    for (int j = 0; j < size; j++)
+//    {
+//        Console.Write($"Enter value of row {i + 1}, col {j + 1}: ");
+//        jagged[i][j] = int.Parse(Console.ReadLine());
+//    }
+//    Console.WriteLine("Jagged Array: ");
+//    foreach (var row in jagged)
+//    {
+//        Console.WriteLine(string.Join(" ", rows));
+//    }
+//}
+
+//3.
+//int[][] jagged =
+//{
+//    new int[] {1, 2, 3},
+//    new int[] {4,5},
+//    new int[] {6, }
+//};
+
+//for (int i = 0; i < jagged.Length; i++)
+//{
+//    int sum = 0;
+//    for (int j = 0; j < jagged[i].Length; j++)
+//    {
+//        sum += jagged[i][j];
+//    }
+//    Console.WriteLine($"Sum of row {i + 1}: {sum}");
+//}
+
+//4. 
+//int[][] jagged =
+//{
+//    new int[] {1, 2, 3},
+//    new int[] {4,5},
+//    new int[] {6, }
+//};
+
+//for (int i = 0; i < jagged.Length; i++)
+//{
+//    Array.Reverse(jagged[i]);
+//}
+//foreach (var row in jagged)
+//{
+//    Console.WriteLine(string.Join(" ", row));
+//}
+//5.
+
+//string text = "Programming";
+//Console.WriteLine(text.Substring(0,4);
+//Console.WriteLine(text.IndexOf('gr'));
+
+//6.
+//string text = "Hello, World!";
+//string result = text.Replace("World", "C#");
+//Console.WriteLine(result);
+
+
+
---
+++ Lecture 2 Lab/Student Grading System/Student Grading System/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of students: ");
+        int numStudents = int.Parse(Console.ReadLine());
+
+        double[][] studentArray = new double[numStudents][];
+        int[] numCourses = new int[numStudents];
+
+        for (int i = 0; i < numStudents; i++)
+        {
+            Console.Write($"Enter the number of courses for student {i + 1}: ");
+            numCourses[i] = int.Parse(Console.ReadLine());
+            studentArray[i] = new double[numCourses[i]];
+
+            for (int j = 0; j < numCourses[i]; j++)
+            {
+                Console.Write($"Enter the grade for course {j + 1} of student {i + 1}: ");
+                studentArray[i][j] = double.Parse(Console.ReadLine());
+
+            }
+
+        }
+        stuAvg(studentArray, numStudents, numCourses);
+
+    }
+    static void stuAvg(double[][] studentArray, int numStudents, int[] numCourses)
+    {
+        for (int i = 0; i < numStudents; i++)
+        {
+            double sum = 0;
+            for (int j = 0; j < numCourses[i]; j++)
+            {
+                sum += studentArray[i][j];
+            }
+            double average = sum / numCourses[i];
+            Console.WriteLine($"Student {i + 1}: Sum = {sum}, Average = {average:F2}");
+        }
+    }
+}
