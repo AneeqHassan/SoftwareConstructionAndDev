6e361e39e387134ba439fcce2f73bf39a5335ddf
2312303 <2312303@PC38>
2025-10-03 11:59:16 +0500

    lec 3

---
+++ Lecture3 Lab/Lecture3 Lab/Program.cs
@@ -0,0 +1,93 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+//Overloaded square function
+
+// double Square(double x) => x * x;
+// Console.WriteLine(Square(5));
+// Console.WriteLine(Square(2.5));
+
+//int Max(int a, int b) => a > b ? a : b;
+//double Max(double a, double b) => a > b ? a : b;
+//Console.WriteLine(Max(3, 7));
+//Console.WriteLine(Max(3.5, 2.5));
+
+// using list
+using System;
+using System.Collections.Generic;
+//class Program
+//{
+//    static void Main()
+//    {
+//        List<int> numbers = new List<int> { 1, 2, 3 };
+//        numbers.Add(4);
+//        foreach (var n in numbers)
+//        {
+//            Console.WriteLine(n);
+//        }
+//    }
+//}
+
+// Using Dictionary
+//using System;
+//using System.Collections.Generic;
+//class Program
+//{
+//    static void Main()
+//    {
+//        Dictionary<string, int> ages = new Dictionary<string, int>();
+//        ages["Ali"] = 20;
+//        ages["Sara"] = 25;
+//        foreach (var i in ages)
+//        {
+//            Console.WriteLine($"{i.Key}: {i.Value}");
+//        }
+//    }
+//}
+
+
+
+//Using Queue
+//using System;
+//using System.Collections.Generic;
+//class Program
+//{
+//    static void Main()
+//    {
+//        Queue<string> q = new Queue<string>();
+//        q.Enqueue("First");
+//        q.Enqueue("Second");
+//        Console.WriteLine(q.Dequeue());
+//    }
+//}
+
+//Using Stack
+//using System;
+//using System.Collections.Generic;
+//class Program
+//{
+//    static void Main()
+//    {
+//        Stack<int> stack = new Stack<int>();
+//        stack.Push(10);
+//        stack.Push(20);
+//        Console.WriteLine(stack.Pop());
+//    }
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        List<int> max = new List<int> { 1, 2, 3, 4, 5 };
+//        Console.WriteLine(max.Max());
+//    }
+//}
+
+//public abstract class Animal
+//{
+//    public abstract void Speak();
+//}
+//public class Dog : Animal
+//{
+//    public override void Speak() => Console.WriteLine("Woof!");
+//}
