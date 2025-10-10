afb4b4455826e24cfaf1b737a8c73b5ff51cd33f
2312303 <2312303@PC38>
2025-10-10 10:23:38 +0500

    ex

---
+++ EntityFrameworkExample0/EntityFrameworkExample0/Model/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using EntityFrameworkExample0.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkExample0.Model
+{
+
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ EntityFrameworkExample0/EntityFrameworkExample0/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EntityFrameworkExample0.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddDbContext<DatabaseContext>(options =>
+            options.UseSqlServer("Data Source=(local);Initial Catalog=TechNova-Ecommerce;Integrated Security=True;Encrypt=False"));
+    })
+    .Build();
+
+
+//Data Source=(local);Initial Catalog=TechNova-Ecommerce;Integrated Security=True;Encrypt=False
+
+
